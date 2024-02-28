3196ebc9af084fcd9c7f56e966ba695a54119985
wzNaji <141943564+wzNaji@users.noreply.github.com>
2024-02-28 18:06:58 +0100

    added model

--- tarik/CrudTodo/src/main/java/com/tarik/crudtodo/model/TodoModel.java
+++ tarik/CrudTodo/src/main/java/com/tarik/crudtodo/model/TodoModel.java
@@ -1,4 +1,33 @@
 package com.tarik.crudtodo.model;
 
 public class TodoModel {
+
+    private String title;
+    private String content;
+
+    public TodoModel(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String toString() {
+        return String.format("Title: %s\nContent: %s",title,content);
+    }
+
 }
