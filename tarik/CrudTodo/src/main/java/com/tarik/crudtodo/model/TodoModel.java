da9152a28ae836961f86c468c754796db79ce7a9
TarikNaji <hallatarik@hotmail.com>
2024-03-04 21:54:56 +0100

    TODO: Give each todo item an unique ID. Remember to hide it in the addForm.html
    AND
    
    Go through the layers and implement the id for each Todo object (as shown in the TodoModel.java)

--- tarik/CrudTodo/src/main/java/com/tarik/crudtodo/model/TodoModel.java
+++ tarik/CrudTodo/src/main/java/com/tarik/crudtodo/model/TodoModel.java
@@ -1,16 +1,21 @@
 package com.tarik.crudtodo.model;
 
+import java.util.Random;
+
 public class TodoModel {
 
     private String title;
     private String content;
+    private static int idCounter = 1;
+    private int id;
 
-    public TodoModel(String title, String content) {
+    public TodoModel(String title, String content, int id) {
         this.title = title;
         this.content = content;
+        this.id = idCounter++;
     }
-    public TodoModel() {
-
+    public TodoModel(int id) {
+        this.id = idCounter++;
     }
 
     public String getTitle() {
